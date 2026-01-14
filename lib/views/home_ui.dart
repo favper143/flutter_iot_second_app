8ff745b66463769ce91036a736ed8e8930e8f4e2
favper143 <s6619410060@sau.ac.th>
2026-01-14 12:16:29 +0700

    Commit 01: ขึ้นโปรเจ็ก หน้าจอ HomeUi เสร็จแล้ว ที่เหลือยังไม่ได้ทำ

---
+++ lib/views/home_ui.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+class HomeUi extends StatelessWidget {
+  const HomeUi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 100,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(100),
+              child: Image.asset(
+                'assets/images/logo (1).png',
+                width: 250,
+                height: 250,
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              height: 80,
+            ),
+            Text(
+              'SIC • PAVIS • MAGNA',
+              style: TextStyle(
+                fontSize: 28.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'greatness from small beginnings',
+              style: TextStyle(
+                fontSize: 13.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            Text(
+              'Created by Sir Francis Drake',
+              style: TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(
+              height: 40,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                OutlinedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'LOG IN',
+                    style: TextStyle(
+                      color: Colors.black,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    fixedSize: Size(
+                      150, 60,),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(
+                      color: Colors.white,),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      150, 60,),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    backgroundColor: Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
