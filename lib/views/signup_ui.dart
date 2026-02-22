32eeea3e149dbd7a9012cf3ef05027fd1e604d60
favper143 <s6619410060@sau.ac.th>
2026-02-23 03:57:45 +0700

    commit 04: ทำเสร็จแล้ว

--- lib/views/signup_ui.dart
+++ lib/views/signup_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SignupUi extends StatefulWidget {
   const SignupUi({super.key});
@@ -11,7 +12,7 @@ class _SignupUiState extends State<SignupUi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(40.0),
@@ -31,7 +32,8 @@ class _SignupUiState extends State<SignupUi> {
                   ),
                 ),
                 SizedBox(
-                  height: 5.0,),
+                  height: 5.0,
+                ),
                 Align(
                   alignment: Alignment.centerLeft,
                   child: Image.asset(
@@ -42,16 +44,16 @@ class _SignupUiState extends State<SignupUi> {
                   ),
                 ),
                 SizedBox(
-                  height: 5.0,),
+                  height: 5.0,
+                ),
                 Align(
                   alignment: Alignment.bottomLeft,
                   child: Text(
                     'Get On Board!',
                     style: TextStyle(
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black
-                    ),
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
                   ),
                 ),
                 Align(
@@ -59,12 +61,170 @@ class _SignupUiState extends State<SignupUi> {
                   child: Text(
                     'Create your prifile to start your journey.',
                     style: TextStyle(
-                      fontSize: 13.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black
+                        fontSize: 13.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.name,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    prefixIcon: Icon(
+                      Icons.person_2_outlined,
+                      color: Colors.grey,
+                    ),
+                    hintText: 'Full Name',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 20.0,
+                      horizontal: 20.0,
                     ),
                   ),
                 ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    prefixIcon: Icon(
+                      Icons.email_outlined,
+                      color: Colors.grey,
+                    ),
+                    hintText: 'E-mail',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 20.0,
+                      horizontal: 20.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                TextField(
+                  keyboardType: TextInputType.phone,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    prefixIcon: Icon(
+                      Icons.phone_outlined,
+                      color: Colors.grey,
+                    ),
+                    hintText: 'Phone No',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 20.0,
+                      horizontal: 20.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0)),
+                    prefixIcon: Icon(
+                      FontAwesomeIcons.fingerprint,
+                      color: Colors.grey,
+                    ),
+                    hintText: 'Password',
+                    hintStyle: TextStyle(color: Colors.grey),
+                    contentPadding: EdgeInsets.symmetric(
+                      vertical: 20.0,
+                      horizontal: 20.0,
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.visibility_off,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    minimumSize: Size(double.infinity, 50.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  child: Text(
+                    'SIGN UP',
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Text(
+                  'OR',
+                ),
+                OutlinedButton.icon(
+                  onPressed: () {},
+                  icon: Image.network(
+                    "https://upload.wikimedia.org/wikipedia/commons/0/09/IOS_Google_icon.png",
+                    width: 30,
+                    height: 30,
+                  ),
+                  label: Text(
+                    "SIGN-IN WITH GOOGLE",
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 50.0),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    side: const BorderSide(color: Colors.grey),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                RichText(
+                  text: TextSpan(
+                    style: const TextStyle(
+                      color: Colors.black54,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    children: [
+                      const TextSpan(text: "Already have an Account? "),
+                      TextSpan(
+                        text: "LOGIN",
+                        style: const TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
